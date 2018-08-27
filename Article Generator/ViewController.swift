1f9db2580cad8977d0f3d438f7c74ca0f3cd9bf7
Charles Zhang <hello@charleszhang.ca>
2018-08-26 22:21:22 -0400

    Initial Commit

---
+++ Article Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Article Generator
+//
+//  Created by Charles Zhang on 2018-08-26.
+//  Copyright © 2018 Firebird Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Article GeneratorTests/Article_GeneratorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Article_GeneratorTests.swift
+//  Article GeneratorTests
+//
+//  Created by Charles Zhang on 2018-08-26.
+//  Copyright © 2018 Firebird Studios. All rights reserved.
+//
+
+import XCTest
+@testable import Article_Generator
+
+class Article_GeneratorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
